ee4a6c4d91933bb33e9cb53db5efccf44b0fa3b0
Fernando Lopez <itsfercodes@outlook.com>
2023-03-10 12:36:10 -0600

    Contries cards displaying added

--- src/countries/Components/CountryCard.tsx
+++ src/countries/Components/CountryCard.tsx
@@ -1,6 +1,25 @@
 import { FC } from "react";
+import Country from "../../types/Country";
 
-const CountryCard: FC<{ country: any }> = ({ country }) => {
-  return <div>I&apos;m {country.name.common}!!</div>;
+const CountryCard: FC<{ country: Country }> = ({ country }) => {
+  const { flags, name, region, population, capital } = country;
+  return (
+    <div>
+      <img alt={flags.alt} src={flags.svg} width={200} />
+      <h2>{name.common}</h2>
+      <p>
+        <strong>Population: </strong>
+        {population}
+      </p>
+      <p>
+        <strong>Region: </strong>
+        {region}
+      </p>
+      <p>
+        <strong>Capital: </strong>
+        {capital}
+      </p>
+    </div>
+  );
 };
 export default CountryCard;
